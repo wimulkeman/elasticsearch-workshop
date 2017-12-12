c9bd1ee656eb414c888d837dde55dd30fd890149
Wim Ulkeman <wimulkeman@hotmail.com>
2017-12-13 00:13:38 +0100

    Progress for exercise 6

--- src/pages/6_type_ahead.php
+++ src/pages/6_type_ahead.php
@@ -1,1 +1,21 @@
-<input type="text" class="typeahead" data-provide="typeahead" autocomplete="off">
+<h2>6: Type ahead</h2>
+
+<?php echo displayExerciseLocationNotice(__FILE__); ?>
+
+<p>
+    One fancy aspect of Elasticsearch is the type ahead functionality.
+</p>
+<p>
+    This exercise contains of one part. Try implementing the type ahead
+    functionality in Elasticsearch. When finished, you should receive a
+    list with options when you start typing in the field below.
+</p>
+<p>
+    <input type="text" class="typeahead" data-provide="typeahead" autocomplete="off">
+</p>
+<p>
+    Wow, if we would mix the exercises together, we could create that
+    Google feeling on our own websites!<br>
+    <?php echo getNextExercisePageHref(__FILE__); ?>
+</p>
+
