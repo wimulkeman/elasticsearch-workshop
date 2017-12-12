828cc9e8479888f1cdc63f6b68eba491de338711
Wim Ulkeman <wim.ulkeman@webstores.nl>
2017-12-12 13:55:48 +0100

    Submitting progress for exercise 5 - Highlighting match phrase 

--- src/pages/5_highlighting_match_phrase.php
+++ src/pages/5_highlighting_match_phrase.php
@@ -1,1 +1,21 @@
-<?php
+<h2>5: Highlighting match phrase</h2>
+
+<?php echo displayExerciseLocationNotice(__FILE__); ?>
+
+<p>
+    We all know Google and the way they show there results. One
+    fancy aspect of that is the way they show what part of the result
+    was a indication to show you the document.
+</p>
+
+<h3>The exercise</h3>
+<p>
+    In this exercise we are going to write a <strong>highlighting</strong>
+    functionality for Elasticsearch which will show which part of a text
+    was mapped by the match.
+</p>
+
+<h3>1.1: Writing our first highlighter</h3>
+<p>
+
+</p>
