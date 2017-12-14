8f7c903ba68f7fccc7f2d0e397b0f2e2432b69f0
Wim Ulkeman <wim.ulkeman@webstores.nl>
2017-12-14 16:55:54 +0100

    Change sentence

--- src/functions.php
+++ src/functions.php
@@ -102,7 +102,7 @@ function getHelpResources(array $resources): string
     $resourceHtml = <<<html
         <h3>Help...</h3>
         <p>
-            Getting stuck? Try these resources:
+            Got stuck? Try these resources:
             <ul>
                 {$links}
             </ul>
