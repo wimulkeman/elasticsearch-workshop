5bc79d5fb0433f5ceac13171732335603f6df0cb
Wim Ulkeman <wim.ulkeman@webstores.nl>
2018-10-13 19:14:27 +0200

    Adding lesson 14 about the possibility to use SQL in Elasticsearch

---
+++ src/pages/14_using_sql_on_indexes.php
@@ -0,0 +1,67 @@
+<h2>14: Using SQL on indexes</h2>
+<p>
+    As of Elasticsearch version 6.3, there is a new feature available which may sound familiar to those
+    working in de development world. Elasticsearch started to introduce SQL support.
+</p>
+<p>
+    Aldo it is still a experimental feature, you can already experiment with how it works and interacts
+    with a existing index, as long as it is running on Elasticsearch 6.3 or higher, which is something
+    wat this workshop is doing.
+</p>
+
+<h3>14.1: Selecting all the records</h3>
+<p>
+    For this exercise you can try to use some of that SQL power for a simple task, retrieving all the records
+    from your index. Try doing that by using <?php echo loadKibanaDevTools(); ?>.
+</p>
+<?php
+echo getHelpResources(
+    [
+        [
+            'url' => 'https://www.elastic.co/products/stack/elasticsearch-sql',
+            'title' => 'Elasticsearch SQL'
+        ],
+        [
+            'url' => 'https://www.elastic.co/guide/en/elasticsearch/reference/6.4/sql-commands.html',
+            'title' => 'Elasticsearch docs: SQL Commands'
+        ],
+        [
+            'url' => 'https://www.elastic.co/guide/en/elasticsearch/reference/current/xpack-sql.html',
+            'title' => 'Elasticsearch docs: SQL Access'
+        ]
+    ]
+);
+?>
+
+<h3>14.2: Shorter</h3>
+<p>
+    That SQL is available doesn't mean that you have to use it. Try retrieving all the records
+    by using the DSL of Elasticsearch in <?php echo loadKibanaDevTools(); ?>. It should be shorter to write.
+</p>
+<?php
+echo getHelpResources(
+    [
+        [
+            'url' => 'https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-all-query.html',
+            'title' => 'Elasticsearch docs: Match All Query'
+        ]
+    ]
+);
+?>
+<p>
+    For now SQL may come in handy for managing your Elasticsearch cluster, but I would recommend it for viewing
+    data in your production environment. That could change in the future as the retrieval through
+    SQL is fast, but less fault proof then the DSL language.
+</p>
+<p>
+    If you want to read more about the usage of SQL within Elasticsearch, you can read there
+    <a href="https://www.elastic.co/blog/an-introduction-to-elasticsearch-sql-with-practical-examples-part-1" target="_blank">blog posts</a>
+    about it.
+</p>
+<p>
+    Well that is it for this exercise.
+</p>
+
+<p>
+    <?php echo getNextExercisePageHref(__FILE__); ?>
+</p>
